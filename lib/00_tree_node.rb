c67c81598ace34a6f4c3986a450ac64b4b0cdc7a
danielariaz-avalos <dannyariaz45@gmail.com>
2022-04-12 12:52:10 -0700

    Created dfs method

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,5 +1,18 @@
 module Searchable
     
+    def dfs(target = nil, &prc)
+        raise "Need a proc or target" if |target, prc]
+        prc ||= Proc.new { |node| node.value == target }
+
+        return self if prc.call(self)
+
+        children.each do |child|
+            result = child.dfs(&prc)
+            return result unless result.nil?
+        end
+        
+        nil
+    end
 
 end
 
